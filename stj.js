20d2e5e8e2412d4719064d7a40182b9e87fad324
001alisson <149392416+001alisson@users.noreply.github.com>
2023-12-18 19:42:45 -0300

    Add files via upload

---
+++ stj.js
@@ -0,0 +1,15 @@
+function animar1(){
+    const ani = document.querySelector(".ct");
+    ani.style.animation = "";
+    setTimeout(() => ani.style.animation = "apa 2s")
+}
+function animar2(){
+    const ani = document.querySelector(".trab");
+    ani.style.animation = "";
+    setTimeout(() => ani.style.animation = "apa 2s")
+}
+function animar3(){
+    const ani = document.querySelector(".cl");
+    ani.style.animation = "";
+    setTimeout(() => ani.style.animation = "apa 2s")
+}
